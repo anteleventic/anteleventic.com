d7e6b14104fdc47546a599f907ecf47fa627e70c
Ante Leventic <ante.leventic@icloud.com>
2025-11-12 15:59:20 +0100

    [REMOVE] Import for Skills.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
-import Skills from "./pages/Skills";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 
